34a09fd31b1e588fd01cbbfc353ea77c99816658
SIMÓN SUKEN ROSENBERG <47296430@ort>
2023-06-05 08:59:46 -0300

    empezamos con la programacion y el index

---
+++ Models/Escape.cs
@@ -0,0 +1,26 @@
+class Escape{
+
+public static string[] incognitasSalas{get; private set;}
+
+public static int estadoJuego{get; set;}
+
+
+static void inicializarJuego(){
+
+string[] incognitasSalas = {"5557", "CENTRAL", "candado3", "AVION"};
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+}
